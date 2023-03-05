1d607481f630c2a68c74b8aadac829d2a01b36f8
Twometer <twometer@outlook.de>
2023-03-05 14:45:51 +0100

    Implement horizontal alignment for math block

--- examples/math.rs
+++ examples/math.rs
@@ -11,6 +11,9 @@ const MATH_EXAMPLE: &'static str = r"
 f^{(n)}(z) = \frac{n!}{2\pi i} \oint \frac{f(\xi)}{(\xi - z)^{n+1}}\,\mathrm{d}\xi
 ";
 
+const MATH_EXAMPLE_SHORT: &'static str =
+    r"\mathop{\overbrace{1+2+3+\unicodecdots+n}}\limits^{\mathrm{Arithmatic}} = \frac{n(n+1)}{2}";
+
 fn make_font_family(data: &[u8]) -> FontFamily<FontData> {
     let font = genpdf::fonts::FontData::new(data.to_vec(), None).unwrap();
     FontFamily {
@@ -32,13 +35,13 @@ fn main() {
     pdf_doc.set_title("genpdf+rex Demo Document");
     pdf_doc.set_minimal_conformance();
     pdf_doc.set_line_spacing(1.25);
-    pdf_doc.set_font_size(30);
+    pdf_doc.set_font_size(10);
 
     let math_font_family = pdf_doc.add_font_family(math_font_family);
     pdf_doc.enable_math(math_font_data, math_font_family);
 
     pdf_doc.push(elements::Text::new("Math with Rex and genpdf"));
-    pdf_doc.push(elements::Math::new(MATH_EXAMPLE));
+    pdf_doc.push(elements::Math::new(MATH_EXAMPLE_SHORT).aligned(genpdf::Alignment::Center));
     pdf_doc.push(elements::Text::new("End of test!"));
 
     pdf_doc
--- src/elements/math.rs
+++ src/elements/math.rs
@@ -1,10 +1,13 @@
+use std::f32::consts::E;
+
 use crate::{
-    math::MathOp, render, style::LineStyle, Context, Element, Position, RenderResult, Size,
+    math::MathOp, render, style::LineStyle, Alignment, Context, Element, Position, RenderResult,
 };
 
 /// An element that can render a MathBlock to a PDF document
 pub struct Math {
     source: String,
+    alignment: Alignment,
 }
 
 impl Math {
@@ -12,7 +15,14 @@ impl Math {
     pub fn new(source: &str) -> Self {
         Self {
             source: source.to_owned(),
+            alignment: Alignment::Left,
         }
+    }
+
+    /// Sets the horizontal alignment of the Math block
+    pub fn aligned(mut self, alignment: Alignment) -> Self {
+        self.alignment = alignment;
+        self
     }
 }
 
@@ -30,12 +40,18 @@ impl Element for Math {
 
         let block = math_renderer.render(style.font_size() as f64, &self.source);
 
+        let x_origin = match self.alignment {
+            Alignment::Left | Alignment::Justified(_) => 0.0,
+            Alignment::Center => (area.size().width / 2.0 - block.size.width / 2.0).0,
+            Alignment::Right => (area.size().width - block.size.width).0,
+        };
+
         for op in block.ops() {
             match op {
                 MathOp::TextSection(text_section) => {
                     area.print_positioned_codepoints(
                         &context.font_cache,
-                        Position::new(0, text_section.origin_y),
+                        Position::new(x_origin, text_section.origin_y),
                         text_section.x_offsets.clone().into_iter(),
                         text_section.glyph_ids.clone().into_iter(),
                         text_section.font_size,
@@ -46,11 +62,11 @@ impl Element for Math {
                 }
                 MathOp::Rule(rule) => area.draw_line(
                     vec![
-                        Position::new(rule.x, rule.y),
-                        Position::new(rule.x + rule.width, rule.y),
-                        Position::new(rule.x + rule.width, rule.y + rule.height),
-                        Position::new(rule.x, rule.y + rule.height),
-                        Position::new(rule.x, rule.y),
+                        Position::new(x_origin + rule.x, rule.y),
+                        Position::new(x_origin + rule.x + rule.width, rule.y),
+                        Position::new(x_origin + rule.x + rule.width, rule.y + rule.height),
+                        Position::new(x_origin + rule.x, rule.y + rule.height),
+                        Position::new(x_origin + rule.x, rule.y),
                     ],
                     LineStyle::default()
                         .with_color(rule.color)
